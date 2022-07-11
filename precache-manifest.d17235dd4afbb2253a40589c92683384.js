cab3d0f4fa88106945ade65546a52c6e77b3bb6b
Rishav Gupta <77620644+iamrishavgupta@users.noreply.github.com>
2022-07-11 19:52:03 +0530

    Updates

---
+++ precache-manifest.d17235dd4afbb2253a40589c92683384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9709efb88ac6f0527c3102259340ea9",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "2dcb501683a974a81a33",
+    "url": "/covid19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "42d07d6bf1655b04e5a7",
+    "url": "/covid19-tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "2dcb501683a974a81a33",
+    "url": "/covid19-tracker/static/js/2.3768dda7.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid19-tracker/static/js/2.3768dda7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42d07d6bf1655b04e5a7",
+    "url": "/covid19-tracker/static/js/main.4e4fe851.chunk.js"
+  },
+  {
+    "revision": "c5535707c668a65b0e80",
+    "url": "/covid19-tracker/static/js/runtime-main.c7a49258.js"
+  }
+]);
